f9f6820c886f735807a3670adc68c2392d018f1c
Martin Rosas <mlrosas21@hotmail.com>
2023-09-13 14:59:21 -0300

    rearrange components orden confirmada

--- pages/confirmacion-compra/index.page.tsx
+++ pages/confirmacion-compra/index.page.tsx
@@ -6,6 +6,8 @@ import ComicCard from "dh-marvel/components/ui/ComicCard/ComicCard";
 import styles from "./styles.module.css";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import CardContent from "@mui/material/CardContent";
+import OrderConfirmed from "dh-marvel/components/ui/OrderConfirmed/OrderConfirmed";
 
 const ConfirmPage = () => {
   const {
@@ -20,62 +22,9 @@ const ConfirmPage = () => {
     }
   }, [comic, buyer, router]);
 
-  if (!comic || !buyer) return;
+  if (!comic || !buyer) return null;
 
-  return (
-    <>
-      <Paper className={styles.root}>
-        <Box className={styles.greenSection}>
-          <Typography variant="h5" className={styles.confirmText}>
-            ¡Que disfrutes tu compra!
-          </Typography>
-        </Box>
-        <Box className={styles.orderSection}>
-          <ComicCard {...comic} />
-          <Box className={styles.personalInfoSection}>
-            <Box>
-              <Typography variant="h5" mb={1}>
-                Datos Personales:
-              </Typography>
-              <Box>
-                <Box className={styles.info}>
-                  <Typography>Nombre completo:</Typography>
-                  <Typography>
-                    {buyer.personalInfo?.name} {buyer.personalInfo?.lastname}
-                  </Typography>
-                </Box>
-                <Typography>Email: {buyer.personalInfo?.email}</Typography>
-              </Box>
-            </Box>
-            <Box>
-              <Typography variant="h5" mb={1}>
-                Dirección de Entrega:
-              </Typography>
-              <Box>
-                <Typography>
-                  Dirección: {buyer.deliveryInfo?.address} -{" "}
-                  {buyer.deliveryInfo?.address2}
-                </Typography>
-                <Typography>
-                  Código postal: {buyer.deliveryInfo?.zip}
-                </Typography>
-                <Typography>Ciudad: {buyer.deliveryInfo?.city}</Typography>
-                <Typography>
-                  Provincia: {buyer.deliveryInfo?.province}
-                </Typography>
-              </Box>
-            </Box>
-            <Box>
-              <Typography variant="h5" mb={1}>
-                Precio final: ${comic.price.toFixed(2)} a debitarse en la
-                tarjeta {buyer.paymentInfo?.number.slice(-4)}{" "}
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-      </Paper>
-    </>
-  );
+  return <OrderConfirmed comic={comic} buyer={buyer} />;
 };
 
 export default ConfirmPage;
